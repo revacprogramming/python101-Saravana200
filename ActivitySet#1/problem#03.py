aace88f972f212aca239abaac811db84cb02b13f
Saravana200 <dhruvassaravana@gmail.com>
2022-05-09 10:19:46 +0530

    p2 and p3

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -1,4 +1,4 @@
-hrs = float(input("Enter hours? "))
-rate=float(input("enter rate: "))
-gross=rate*hrs
-print("gross    ")
+hrs = float(input("Enter Hours:"))
+rate=float(input("enter rate:"))
+gross=hrs*rate
+print("Pay:",gross)
