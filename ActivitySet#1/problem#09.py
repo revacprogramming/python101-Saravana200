d451e0fa6dd0b829a9cd52b311c72475226eba94
Dcsk <96806975+Saravana200@users.noreply.github.com>
2022-05-12 12:51:01 +0000

    p10 and p9

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,8 @@
-# Lists
-
-filename = "dataset/romeo.txt"
+l=[]
+with open("romeo.txt","r") as file:
+    for i in file:
+        for word in i.split():
+            if (word in l)==False:
+                l.append(word)
+l.sort()
+print(l)
--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,7 @@
-# Dictionaries
-
-filename = "dataset/mbox-short.txt"
+count=0
+with open("mbox-short.txt","r") as file:
+    for i in file:
+        if(i.split(' ')[0]=="From" and len(i.split())>2):
+            print(i.split()[1])
+            count+=1
+print("There were", count, "lines in the file with From as the first word")
