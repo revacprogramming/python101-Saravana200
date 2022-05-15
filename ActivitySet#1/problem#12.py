041fd8400ff613f6a13d11ae0dfd6c23bbf6aca0
Dcsk <96806975+Saravana200@users.noreply.github.com>
2022-05-15 03:45:10 +0000

    15/05/22

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,11 @@
-# Regular Expressions
-# https://www.py4e.com/lessons/rege
+a={}
+with open("mbox-short.txt","r") as f:
+    for i in f:
+        if(i.startswith("From") and len(i.split())>3):
+            t=(i.split()[5]).split(':')[0]
+            a[t]=a.get(t,0)+1
+l=list(a.items())
+l.sort()
+a=dict(l)
+for i in a:
+    print(i,a[i],sep=' ')
